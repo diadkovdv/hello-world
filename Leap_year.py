f8aa3b83c8ee71c6dcd01c2f3f8c34ce7ad27438
SantaCloser <dyadkov@mail.ru>
2021-07-13 17:37:47 +0300

    Change file to master branch, input modification

--- Leap_year.py
+++ Leap_year.py
@@ -1,7 +1,7 @@
-x = int(input())
+x = int(input('Enter the year: '))
 
 # def is_leap_year(x):
-if (x%4 == 0 and x%100 != 0) or (x%400 == 0):
+if (x % 4 == 0 and x % 100 != 0) or (x % 400 == 0):
     print('True')
 else:
     print( 'False')        
