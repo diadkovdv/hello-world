868ce3144b8bd0921534415dd1052d7d592af7b9
SantaCloser <dyadkov@mail.ru>
2021-07-13 18:18:40 +0300

    Modified output messages

--- Leap_year.py
+++ Leap_year.py
@@ -1,7 +1,6 @@
 x = int(input('Enter the year: '))
 
-# def is_leap_year(x):
 if (x % 4 == 0 and x % 100 != 0) or (x % 400 == 0):
-    print('True')
+    print('Високосный год.')
 else:
-    print( 'False')        
+    print( 'Обычный год, слава Богу!')        
